c545bea7602cc2036a368f46c5ca51fc63e358e1
Kevin Briggs <kevinbriggs1@gmail.com>
2019-10-19 22:07:34 -0400

    added toggle and spread

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,31 +1,70 @@
-import React, {useEffect, useState} from "react";
-import { get } from "https";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import Switch from "react-switch";
+
+const Score = styled.span`
+  margin: 10px;
+`;
+
+const ScoreContainer = styled.div`
+  margin-bottom: 10px;
+`;
+
+const Spread = styled.input`
+  width: 30px;
+`;
+
+const Pick = styled(Switch)`
+  width: 40px;
+`;
 
 const App = props => {
-  
   const [games, setGames] = useState([]);
+  const [spreads, setSpreads] = useState({});
 
-  useEffect(()=> {
+  useEffect(() => {
     getGames();
-  }, [])
-  
+  }, []);
+
   function getGames() {
-    fetch("/api/get-games")
-      .then(resp => resp.json()) // Transform the data into json
-      .then(function(data) {
-        setGames(data)
-      });
+    axios("/api/get-games").then(function(res) {
+      setGames(res.data);
+    });
+  }
+
+  function handleSpreadChange(e, id) {
+    setSpreads({ ...spreads, [id]: e.target.value });
   }
 
   function mapGames() {
-    return games.map((game)=> {
+    return games.map(game => {
       return (
-      <div>
-      <p>{game.away_team}: {game.away_score}</p>
-      <p>{game.home_team}: {game.home_score}</p>
-      </div>
-      )
-    })
+        <ScoreContainer>
+          <Score>
+            {game.away_team}:&nbsp;
+            {game.away_score}
+          </Score>
+          <Score>
+            {game.home_team}:&nbsp;
+            {game.home_score}
+          </Score>
+          <Spread
+            onChange={e => handleSpreadChange(e, game.game_id)}
+            value={spreads[game.game_id]}
+          />
+          <Pick
+            uncheckedIcon={false}
+            checkedIcon={false}
+            checked={true}
+            onChange={() => togglePick}
+            handleDiameter={15}
+            width={35}
+            height={20}
+          />
+        </ScoreContainer>
+      );
+    });
   }
 
   return <div>{mapGames()}</div>;
